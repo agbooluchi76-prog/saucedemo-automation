decc6dc47f306ef2d73b298544752203f62b8de9
Felicia Agbo <agbooluchi76@gmail.com>
2026-08-23 14:58:14 +0100

    config: use baseURL instead of hardcoded URLs

--- auth.setup.js
+++ auth.setup.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 (async () => {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.saucedemo.com');
+  await page.goto(process.env.BASE_URL);
   await page.locator('#user-name').fill(process.env.SAUCE_USERNAME);
   await page.locator('#password').fill(process.env.SAUCE_PASSWORD);
   await page.locator('#login-button').click();
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -8,8 +8,8 @@ class LoginPage {
   }
 
   async goto() {
-    await this.page.goto('https://www.saucedemo.com');
-  }
+  await this.page.goto('/');
+}
 
   async login(username, password) {
     await this.usernameInput.fill(username);
